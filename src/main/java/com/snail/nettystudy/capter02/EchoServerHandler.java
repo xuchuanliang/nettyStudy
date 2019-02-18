5858e5fbce9a3b93fe2125eb48c2337526415500
xuchuanliangbt <xuchuanliangbt@hikbt.com>
2019-02-18 12:33:07 +0800

    提交

---
+++ src/main/java/com/snail/nettystudy/capter02/EchoServerHandler.java
@@ -0,0 +1,54 @@
+package com.snail.nettystudy.capter02;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.CharsetUtil;
+
+/**
+ * @ChannelHandler.Sharable ：标识一个ChannelHandler可以被多个Channel安全的共享
+ * 响应传入的消息
+ */
+@ChannelHandler.Sharable
+public class EchoServerHandler extends ChannelInboundHandlerAdapter {
+
+    /**
+     * 对于每个传入的消息都要调用
+     * @param ctx
+     * @param msg
+     * @throws Exception
+     */
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ByteBuf in = (ByteBuf) msg;
+        System.out.println("Server received:"+in.toString(CharsetUtil.UTF_8));
+        //将接收到的消息写给发送者，而不冲刷出站消息
+        ctx.write(in);
+    }
+
+    /**
+     * 通知ChannelInboundHandler最后一次对channelRead的调用是当前批量读取读取中的最后一条消息
+     * @param ctx
+     * @throws Exception
+     */
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        //将未决消息冲刷到远程节点，并且关闭该Channel
+        ctx.writeAndFlush(Unpooled.EMPTY_BUFFER).addListener(ChannelFutureListener.CLOSE);
+    }
+
+    /**
+     * 在读取操作期间，有异常抛出时会调用
+     * @param ctx
+     * @param cause
+     * @throws Exception
+     */
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
