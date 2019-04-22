38e6128e49eb80520a7ff95afe87ba89a5b1bde5
xuchuanliang <xuchuanliang361@163.com>
2019-04-22 08:21:35 +0800

    攻坚回来重新学习

--- src/test/java/com/snail/nettystudy/NettyStudyApplicationTests.java
+++ src/test/java/com/snail/nettystudy/NettyStudyApplicationTests.java
@@ -5,12 +5,19 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class NettyStudyApplicationTests {
 
     @Test
     public void contextLoads() {
+        System.out.println(Collections.EMPTY_LIST.contains("111"));
     }
 
 }
